dafd68d37f66e13c459172a106a683ec6b17954d
nefri90 <mitchenko9@yandex.ru>
2017-01-24 16:33:22 +0300

    функция и вызов html

---
+++ untitled.js
@@ -0,0 +1,18 @@
+var arr = [0, -3, 0, 0, 0, 6, 5, 3, 7, 0, 0,];
+
+function moveZero (arr){
+    var a = 0;
+
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] == 0) {
+            a++;
+        } else {
+            if (a > 0) {
+                arr[i - a] = arr[i];
+                arr[i] = 0;
+            }
+        }
+    }
+}
+
+alert (moveZero (arr));
